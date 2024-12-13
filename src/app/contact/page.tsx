5b38b52a67da90369b87a51a9ec4e0a6f1cd357e
svivek19 <svivek030503@gmail.com>
2024-12-13 18:26:03 +0530

    contact page fixed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import { BsWhatsapp } from "react-icons/bs";
+
 export default function page() {
-  return <div>Page</div>;
+  return (
+    <div className="flex flex-col space-y-8 justify-center items-center h-[60vh]">
+      <div className="flex justify-center my-6">
+        <h1 className="text-4xl font-semibold font-mono text-gray-700">
+          Contact Here!
+        </h1>
+      </div>
+      <div
+        onClick={() =>
+          window.open(`https://wa.me/${process.env.NEXT_PUBLIC_PHONE_NUMBER}`)
+        }
+        className="text-green-500 cursor-pointer border-2 border-green-500 p-3 rounded-full hover:border-4 duration-300 shadow-xl"
+      >
+        <BsWhatsapp size={35} />
+      </div>{" "}
+    </div>
+  );
 }
